56ad752d5521b1e095a57c30754e902e14dd5bba
test <test>
2021-02-25 11:29:01 +0900

    필요없는 부분 제거

--- android/app/src/main/java/com/betrider/kakaomap/MainActivity.java
+++ android/app/src/main/java/com/betrider/kakaomap/MainActivity.java
@@ -1,17 +1,7 @@
 package com.betrider.kakaomap;
 
-import android.os.Bundle;
-
 import io.flutter.embedding.android.FlutterActivity;
 
-import com.yoonjaepark.flutter_kakao_map.FlutterKakaoMapPlugin;
-
-import android.os.Build;
-import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 }
+

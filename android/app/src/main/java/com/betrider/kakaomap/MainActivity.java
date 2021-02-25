11db511b35660c9bdaa22d8db07cec9b3be8e01a
test <test>
2021-02-25 11:22:57 +0900

    first commit

---
+++ android/app/src/main/java/com/betrider/kakaomap/MainActivity.java
@@ -0,0 +1,17 @@
+package com.betrider.kakaomap;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+import com.yoonjaepark.flutter_kakao_map.FlutterKakaoMapPlugin;
+
+import android.os.Build;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
